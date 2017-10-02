0f65fc71f2ee7db7901130c015405f1839149cef
Nicolas SAID <nicolas.said@novadem.com>
2017-10-02 17:20:27 +0200

    dffd

--- include/environment.h
+++ include/environment.h
@@ -1,1 +1,27 @@
 #pragma once
+
+#include <tiny_dnn/tiny_dnn.h>
+
+class Environment
+{
+public:
+    Environment(){}
+    virtual ~Environment(){}
+
+    // Perform action, return new state and reward
+    double step( const tiny_dnn::Tensor& action, tiny_dnn::Tensor& new_state )
+    {
+        perform_action( action );
+        new_state = state();
+        return reward;
+    }
+
+    virtual double reward(){ return 0.0; }
+    virtual tiny_dnn::Tensor state(){ return tiny_dnn::Tensor(); }
+    virtual void perform_action( tiny_dnn::Tensor& action ){}
+
+private:
+
+protected:
+
+};
--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,9 @@
 #include <iostream>
 
+#include <tiny_dnn/tiny_dnn.h>
+
 using namespace std;
+using namespace tiny_dnn;
 
 int main( int argc, char** argv )
 {
